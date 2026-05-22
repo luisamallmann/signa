492b29778511fe63b68b5ec92e8fef3c8df5dd52
Luísa Mallmann <164959727+luisamallmann@users.noreply.github.com>
2026-05-22 10:20:53 -0300

    Adding header file via upload

---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,21 @@
+export default function Header(){
+    return(
+        <header className="header">
+            <div className="logo">
+                <img></img>
+            </div>
+
+            <nav>
+                <ul className="menu">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/tradutor">Tradutor</a></li>
+                    <li><a href="/como-funciona">Como Funciona</a></li>
+                    <li><a href="/sobre">Sobre</a></li>
+                    <li><a href="/contato">Contato</a></li>
+                </ul>
+            </nav>
+
+            <div></div>
+        </header>
+    )
+}
