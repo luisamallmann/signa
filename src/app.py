40f2cb07299219f16a85bf45faed5ad5baff2594
Luísa Mallmann <164959727+luisamallmann@users.noreply.github.com>
2026-06-10 10:47:16 -0300

    Adicionando app.py via upload

---
+++ src/app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from routes.auth import auth
+
+app = Flask(__name__)
+
+app.register_blueprint(auth, url_prefix='auth')
+
+if __name__ == '__main__':
+    app.run(debug=True)
