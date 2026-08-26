706a995f853799d3b7d7afb05a72c6ed8c0a7aff
Luísa Mallmann <164959727+luisamallmann@users.noreply.github.com>
2026-08-26 08:53:28 -0300

    Add routes for password and email change
    Added routes for changing password and email.

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -10,3 +10,13 @@ def fazer_login():
 @auth.route('/cadastro', methods=['GET', 'POST'])
 def fazer_cadastro():
     return cadastro()
+
+@auth.route('/alterar-senha', methods=['GET', 'POST'])
+def alterar_senha():
+    return alt_senha()
+
+@auth.route('/alterar-email', methods=['GET', 'POST'])
+def alterar_email():
+    return alt_email()
+
+# fazer funcoes de alterar senha e alterar email
